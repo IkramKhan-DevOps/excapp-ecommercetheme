a1970a9375d8ac4aeca7e14c6436ead46e1c1368
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2022-02-10 02:58:41 +0500

    PROJECT: validations added to signup and signin

---
+++ js/my.js
@@ -0,0 +1,21 @@
+
+// FUNCTION:: check if the value is empty/null
+function isNullOrEmpty(value) {
+    if (value == null || value == '' || value.length === 0) {
+        return true
+    }
+    return false
+}
+
+// FUNCTION:: check if the value is stored as null/empty
+function isEmptyNullOrNoStorage(value) {
+    if (window.localStorage.getItem(value) === null || window.localStorage.getItem(value) === '') {
+        return true
+    }
+    return false
+}
+
+// FUNCTION:: get stored value
+function getStoredValue(value) {
+    return window.localStorage.getItem(value)
+}
